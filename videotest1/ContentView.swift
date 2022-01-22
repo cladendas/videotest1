0e7dffc49710affb8e8aab565fad5579ca28c514
Vadim <rvdagi05@yandex.ru>
2022-01-23 01:36:12 +0300

    Initial Commit

---
+++ videotest1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  videotest1
+//
+//  Created by cladendas on 23.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ videotest1/videotest1App.swift
@@ -0,0 +1,17 @@
+//
+//  videotest1App.swift
+//  videotest1
+//
+//  Created by cladendas on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct videotest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
